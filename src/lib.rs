87e8b1d376e42161dec47748766d2df2d8deab72
hengchu <hengchu.zhang@gmail.com>
2019-07-21 03:36:41 -0400

    filling out more bits for easy testing in python

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(specialization)]
+
 use pyo3::prelude::*;
 use pyo3::PyNumberProtocol;
 use pyo3::types::PyAny;
@@ -7,12 +9,32 @@ use pyo3::types::PyAny;
 struct BinaryArithmetic {}
 
 #[pyproto]
-impl PyNumberProtocol for BinaryArithmetic {
-    fn __add__(lhs: &PyAny, rhs: &PyAny) -> PyResult<String> {
-        Ok(format!("{:?} + {:?}", lhs, rhs))
+impl<'p> PyNumberProtocol<'p> for BinaryArithmetic {
+    fn __add__(lhs: &PyAny, rhs: &PyAny) -> PyResult<PyObject> {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        if !py.is_instance::<BinaryArithmetic, _>(lhs).unwrap() {
+            return Ok(py.NotImplemented())
+        }
+        Ok(format!("{:?} + {:?}", lhs, rhs).into_object(py))
     }
 
     fn __radd__(&self, other: &PyAny) -> PyResult<String> {
+        panic!("called");
         Ok(format!("{:?} + {:?}", self, other))
     }
+}
+
+#[pymethods]
+impl BinaryArithmetic {
+    #[new]
+    fn new(obj: &PyRawObject) {
+        obj.init(BinaryArithmetic {})
+    }
+}
+
+#[pymodule]
+fn pyo3_bug(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<BinaryArithmetic>().unwrap();
+    Ok(())
 }
