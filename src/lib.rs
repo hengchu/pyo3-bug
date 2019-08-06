de49d2eee81bd89cfcae1bed881efb23e5f88b17
hengchu <hengchu.zhang@gmail.com>
2019-08-06 15:08:38 -0400

    PyRef type error issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,51 @@
-#![feature(specialization)]
-
 use pyo3::prelude::*;
-use pyo3::PyNumberProtocol;
-use pyo3::types::PyAny;
+use pyo3::wrap_pyfunction;
 
 #[pyclass]
 #[derive(Debug)]
-struct BinaryArithmetic {}
+struct FooBar {}
 
-#[pyproto]
-impl<'p> PyNumberProtocol<'p> for BinaryArithmetic {
-    fn __add__(lhs: &PyAny, rhs: &PyAny) -> PyResult<PyObject> {
-        let gil = Python::acquire_gil();
-        let py = gil.python();
-        if !py.is_instance::<BinaryArithmetic, _>(lhs).unwrap() {
-            return Ok(py.NotImplemented())
-        }
-        Ok(format!("{:?} + {:?}", lhs, rhs).into_object(py))
-    }
-
-    fn __radd__(&self, other: &PyAny) -> PyResult<String> {
-        panic!("called");
-        Ok(format!("{:?} + {:?}", self, other))
-    }
-}
-
-#[pymethods]
-impl BinaryArithmetic {
-    #[new]
-    fn new(obj: &PyRawObject) {
-        obj.init(BinaryArithmetic {})
-    }
+#[pyfunction]
+fn say_hello(_py: Python, _foobar: &FooBar) -> PyResult<String> {
+    Ok("hello".to_string())
 }
 
 #[pymodule]
-fn pyo3_bug(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<BinaryArithmetic>().unwrap();
+fn pyo3_bug(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<FooBar>().unwrap();
+    m.add_wrapped(wrap_pyfunction!(say_hello));
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+
+        let foobar = FooBar {};
+        let code = r#"
+import sys
+
+sys.path.append('.')
+
+from pyo3_bug import say_hello
+
+print(say_hello(foobar))
+            "#;
+
+        let main_module = py.import("__main__").unwrap();
+        let globals = main_module.dict().copy().unwrap();
+        globals
+            .set_item("foobar", PyRef::new(py, foobar).unwrap())
+            .unwrap();
+
+        py.run(code, Some(globals), None).map_err(|e| {
+            e.print(py);
+            assert!(false);
+        });
+    }
 }
